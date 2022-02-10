45a91cba6627d207222505c86b1716cec2a1d66b
Jesus Rdoriguez <JesusRodAltamar@gmail.com>
2022-02-09 23:00:52 -0500

    Funcoines flecha

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-// ES5 - Variables
-var nombre = "Jesus";
-console.log(nombre);
-nombre = "Francsico";
-console.log(nombre);
+//Funciones flecha
+//Funciones en ES5
 
-// ES5 - Variables
-const nombre6 = "Daniela";  //variable const es para uso de solo lectura, la variable no puede cambiar de valor
-let edad = 35;  //Lavariable puede cambiar de valor
-console.log(nombre6);
-console.log(edad);
-edad = 40;
-console.log(edad);
+const years = [2000, 2005, 2008, 2012];
+
+var edad5 = years.map(function(el){
+    return 2019 - el;
+})
+console.log(edad5);
+
+//funcion en ES6 (TIPO FLECHA)
+let edad6 = years.map(el=>{
+    return 2019-el;
+})
+console.log(edad6);
